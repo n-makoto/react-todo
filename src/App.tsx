8cdbe3cdad9541887c070ec633a5f36eb9ac3979
nakagawa.makoto <wwwww.desire@gmail.com>
2021-04-05 23:55:36 +0900

    feature: add/remove task. and implement button that can delete all task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [tasks, setTasks] = useState(["Greet your neighbor", "Eat breakfast"]);
+
+  const onTaskInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+  const addTask = () => {
+    const alreadyExistSameTask = tasks.some((task) => task === inputText);
+    if (alreadyExistSameTask) return false;
+    const newTasks = [...tasks, inputText];
+    setTasks(newTasks);
+    setInputText("");
+  };
+  const addTaskIfEnterPressed = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      addTask();
+    }
+  };
+  const deleteTask = (i: number) => {
+    const newTasks = tasks.filter((_task, index) => index !== i);
+    setTasks(newTasks);
+  };
+  const deleteAllTask = () => {
+    setTasks([]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>ToDoApp</h1>
+        <p>Please input your task!</p>
+        <div className="input-container">
+          <input
+            type="text"
+            value={inputText}
+            onChange={onTaskInput}
+            onKeyPress={addTaskIfEnterPressed}
+          />
+          <button onClick={addTask}>add</button>
+        </div>
+        <ul className="todo-list">
+          {tasks.map((task, index) => (
+            <li key={task}>
+              <span>{task}</span>
+              <span className="delete-btn" onClick={() => deleteTask(index)}>
+                {" "}
+                ❎
+              </span>
+            </li>
+          ))}
+          {tasks.length > 0 && (
+            <button onClick={deleteAllTask}>DELETE ALL TASK</button>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
